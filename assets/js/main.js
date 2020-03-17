8347b02f29c9f5768420f840ca618f4545c92a35
danica <danicastankovic86@gmail.com>
2020-03-17 20:52:24 +0100

    Izmena dropdown-a, modal-a i upload slike.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
 
     });
+   
 //    SEARCH FOR NAME
 
 
@@ -21,38 +22,98 @@ $(document).ready(function () {
     $('.delete-btn').on('click', function(event) {
         $(this).parents('tr').remove();
     });
-    // $(".edit-btn").click(function(){
-    //     $("#myModal").modal();
-    // });
 
-
-
-// For Upload Image
-$('input[type="file"]').change(function() {
-    $('.preview').html('');
-
-    $.each(this.files, function() {
-      readURL(this);
-    })
-  });
-  
-  function readURL(file) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        $('.center-div').html('   <span>LOADEWR</span>')
-     
-      $('.preview').append('<img src=' + e.target.result + ' style="width: 100%; height: 100%;"/>');
+    $(".edit-btn").each(function(){
+        $(this).on('click', function(){
+            event.preventDefault();
+            $("#add-new-tag").modal();
+        })
+   
+    });
+function validateField(){
+    let name = $('#name').val();
+    if (name === "" || name === null) {
+        $('#name').addClass('error-border');
+        return false;
+    } else {
+        $('#name').removeClass('error-border');
     }
-  
-    reader.readAsDataURL(file);
-  }
+}
+$('.btn-save').on('click', function(){ validateField();
+
+    let name = $('#name').val();
+    let result = $('#checkbox-feed').is(":checked");
+    let result2 = $('#checkbox-favorites').is(":checked");
+    let selectedval =   $('#select-tag :selected').text();
+   
+    $('.tag-table tr:last').after(`<tr class="content"><td></td><td class="tag-name"><a href="#">${name}</a></td><td>${selectedval}</td><td class='${result ? "feed" : ""}'></td><td class='${result2 ? "favorite" : ""}'></td><td class="option-box"><button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button></td></tr>`)
+})
+
+
 
 });//document
+function filterText()
+	{  
+        var rex = new RegExp($('#feed').val());
+        
+        if(rex ==="all"){
+            clearFilter()
+                console.log("all")
+            }
+        else{
+			$('.content').hide();
+			$('.content').filter(function() {
+			return rex.test($(this).text());
+			}).show();
+	}
+	}
+	
+function clearFilter()
+	{
+        alert("gafafs")
+        // $('.filterText').val('');
+		// $('.content').show();
+	}
+// Upload image
+
+function dragNdrop(event) {
+    document.getElementsByClassName("progress-bar-wrapper")[0].style.display = "block";
+    document.getElementsByClassName("hide-div")[0].style.display = "none";
+    setTimeout(function(){
+        var fileName = URL.createObjectURL(event.target.files[0]);
+        var preview = document.getElementById("preview");
+        var previewImg = document.createElement("img");
+        previewImg.setAttribute("src", fileName);
+        previewImg.classList.add("myimg");
+        preview.innerHTML = "";
+        preview.appendChild(previewImg);
+    }, 2000);
+  
+ 
 
 
+}
+function drag() {
+    document.getElementById('upload-file').parentNode.className = 'draging drag-box';
+    console.log("drag");
+
+}
+function drop() {
+    document.getElementById('upload-file').parentNode.className = 'drag-box';
+    console.log("drop");
+}
+function addImageInPlaceholder() {
+    // for append in placeholder
+        var img = document.createElement("img");
+        var myimg = document.getElementsByClassName("myimg")[0];
+        img.src = myimg.src;
+        var src = document.getElementById("logo-placeholder");
+        src.innerHTML = "";
+        src.appendChild(img);
+}
 
 
-let dataObj, i, results = "";
+let dataObj, results = "";
 dataObj = {
     data: [
         {
@@ -67,7 +128,6 @@ dataObj = {
             "id": "2",
             "name": "Fudbal",
             "type": "",
-            'feed': "",
            "favorites": false
         },
         {
@@ -149,9 +209,9 @@ dataObj = {
         }
     ]
 }
+var i = 1
 for (i in dataObj.data) {
-
-    results += (`<tr class=""><td>${dataObj.data[i].id}</td><td class="tag-name"><a href="#">${dataObj.data[i].name}</a></td><td>${dataObj.data[i].type}</td><td class='${dataObj.data[i].class === "feed" ? "feed" : ""}' feed=${dataObj.data[i].feed}></td><td class='${dataObj.data[i].favorites === true ? "favorite": ""}'></td><td class="option-box"><button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button></td></tr>`)
+    results += (`<tr class="content"><td>${i}</td><td class="tag-name"><a href="#">${dataObj.data[i].name}</a></td><td>${dataObj.data[i].type}</td><td class='${dataObj.data[i].class === "feed" ? "feed" : ""}' >${dataObj.data[i].class === "feed" ? "yes" : "no"}</td><td class='${dataObj.data[i].favorites === true ? "favorite": ""}'></td><td class="option-box"><button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button></td></tr>`)
 }
 
 $('table').find('tbody').html(results);
