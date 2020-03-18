a5fbdf085ed41afbdd8906943b1aadd12115ef5c
danica <danicastankovic86@gmail.com>
2020-03-18 21:46:38 +0100

    Finalna verzija.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,148 @@
 $(document).ready(function () {
-
-    $('#table_id').dataTable({
-        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
-
+    // DataTable initialisation
+    $('#table_id').DataTable({
+        "paging": true,
+        "autoWidth": true,
+        "pagingType": "full_numbers",
+        responsive: true
     });
-   
-//    SEARCH FOR NAME
+
+    //    SEARCH FOR NAME
+    $("#fn-search-tag-name").keyup(function () {
+        $this = $(this);
+        $.each($("#table_id  tbody tr"), function () {
+            if ($(this).text().toLowerCase().indexOf($($this).val().toLowerCase()) === -1) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        });
+    });
+    //    SEARCH FOR TAG
+    $("#fn-search-tag-type").keyup(function () {
+        $this = $(this);
+        $.each($("#table_id  tbody tr"), function () {
+            if ($(this).text().toLowerCase().indexOf($($this).val().toLowerCase()) === -1) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        });
+    });
 
 
     // SEARCH FOR ID
 
-    $("#fn-sarch-tag-id").keyup(function() {
+    $("#fn-sarch-tag-id").keyup(function () {
         var value = this.value;
-        $("table").find("tr").each(function(index) {
+        $("table").find("tr").each(function (index) {
             if (index === 0) return;
             var id = $(this).find("td").first().text();
-                console.log(id)
+            console.log(id)
             $(this).toggle(id.indexOf(value) !== -1);
         });
     });
-    $('.delete-btn').on('click', function(event) {
+
+    // DELETE ROW
+    $('.delete-btn').on('click', function (event) {
         $(this).parents('tr').remove();
     });
-
-    $(".edit-btn").each(function(){
-        $(this).on('click', function(){
-            event.preventDefault();
-            $("#add-new-tag").modal();
-        })
-   
-    });
-function validateField(){
-    let name = $('#name').val();
-    if (name === "" || name === null) {
-        $('#name').addClass('error-border');
-        return false;
-    } else {
-        $('#name').removeClass('error-border');
+    // VALIDATE NAME FIELD
+    function validateField() {
+        let name = $('#name').val();
+        if (name === "" || name === null) {
+            $('#name').addClass('error-border');
+            return false;
+        } else {
+            $('#name').removeClass('error-border');
+        }
     }
-}
-$('.btn-save').on('click', function(){ validateField();
-
-    let name = $('#name').val();
-    let result = $('#checkbox-feed').is(":checked");
-    let result2 = $('#checkbox-favorites').is(":checked");
-    let selectedval =   $('#select-tag :selected').text();
+    $('.btn-save').on('click', function () {
+        validateField();
+        let name = $('#name').val();
+        let result = $('#checkbox-feed').is(":checked");
+        let result2 = $('#checkbox-favorites').is(":checked");
+        let selectedval = $('#select-tag :selected').text();
+        var id =  $('#table_id tr').last()
+       
+        console.log(id)
+        $('#table_id tr:last').after(`<tr class="content"><td></td><td class="tag-name" ><a href="#">${name}</a></td><td>${selectedval}</td><td class='${result ? "feed" : "no-feed"}'></td><td class='${result2 ? "favorite" : ""}'></td><td class="option-box"><button class="edit-btn"></button> <button class="delete-btn"></button></td></tr>`)
+        var oTable = $('#table_id').dataTable();
+        oTable.fnPageChange( 'last' );
+        $('#add-new-tag').modal('hide');
+    });
+    // refreshPage
+    function refreshPage(){
+        window.location.reload();
+    } 
+    $('.btn-save-modal').on('click', function () {
+        refreshPage()
+      
+    })
+    // EDIT ROW
    
-    $('.tag-table tr:last').after(`<tr class="content"><td></td><td class="tag-name"><a href="#">${name}</a></td><td>${selectedval}</td><td class='${result ? "feed" : ""}'></td><td class='${result2 ? "favorite" : ""}'></td><td class="option-box"><button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button></td></tr>`)
-})
-
-
+    $(".edit-values").on('click', function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        $('#myModal').modal();
+        $(this).closest('tr').find('td.editable-columns').each(function () {
+            var html = $(this).html();
+            console.log(html)
+            var currentValue = $(this).text();
+            console.log(currentValue);
+            let test = ("<input class='editable-column-style' type='text' value=" + currentValue + " />")
+            $('.modal-body').append(test)
+            $('.btn-danger').html('Save')
+           
+        });
+    });
+    // ***************
+    $('.tag-name a').on('click', function(e){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        $('#myModal').modal();
+        $(this).closest('tr').find('td.editable-columns').each(function () {
+            var html = $(this).html();
+            console.log(html)
+            var currentValue = $(this).text();
+            console.log(currentValue);
+            let test = ("<input class='editable-column-style' type='text' value=" + currentValue + " />")
+            $('.modal-body').append(test)
+            $('.btn-danger').html('Save')
+            $('.btn-danger').on('click', function () {
+                $('table').find('tbody').html("Dddd");
+            })
+        });
+    })
 
 });//document
-function filterText()
-	{  
-        var rex = new RegExp($('#feed').val());
-        
-        if(rex ==="all"){
-            clearFilter()
-                console.log("all")
-            }
-        else{
-			$('.content').hide();
-			$('.content').filter(function() {
-			return rex.test($(this).text());
-			}).show();
-	}
-	}
-	
-function clearFilter()
-	{
-        alert("gafafs")
-        // $('.filterText').val('');
-		// $('.content').show();
-	}
+// FILTER
+function filterText() {
+    var rex = new RegExp($('#feed').val());
+    if (rex == "/all-feed/") {
+        clearFilter()
+    } else {
+        $('.content').hide();
+        $('.content').filter(function () {
+            return rex.test($(this).text());
+        }).show();
+    }
+}
+
+function clearFilter() {
+    $('.content').show();
+}
+
+
+
 // Upload image
 
 function dragNdrop(event) {
     document.getElementsByClassName("progress-bar-wrapper")[0].style.display = "block";
     document.getElementsByClassName("hide-div")[0].style.display = "none";
-    setTimeout(function(){
+    setTimeout(function () {
         var fileName = URL.createObjectURL(event.target.files[0]);
         var preview = document.getElementById("preview");
         var previewImg = document.createElement("img");
@@ -88,8 +151,8 @@ function dragNdrop(event) {
         preview.innerHTML = "";
         preview.appendChild(previewImg);
     }, 2000);
-  
- 
+
+
 
 
 }
@@ -104,12 +167,12 @@ function drop() {
 }
 function addImageInPlaceholder() {
     // for append in placeholder
-        var img = document.createElement("img");
-        var myimg = document.getElementsByClassName("myimg")[0];
-        img.src = myimg.src;
-        var src = document.getElementById("logo-placeholder");
-        src.innerHTML = "";
-        src.appendChild(img);
+    var img = document.createElement("img");
+    var myimg = document.getElementsByClassName("myimg")[0];
+    img.src = myimg.src;
+    var src = document.getElementById("logo-placeholder");
+    src.innerHTML = "";
+    src.appendChild(img);
 }
 
 
@@ -122,13 +185,13 @@ dataObj = {
             "type": "",
             'feed': "",
             "class": "feed",
-           "favorites": false
+            "favorites": false
         },
         {
             "id": "2",
             "name": "Fudbal",
             "type": "",
-           "favorites": false
+            "favorites": false
         },
         {
             "id": "3",
@@ -143,7 +206,7 @@ dataObj = {
             "id": "4",
             "name": "premier league",
             "type": "Takmicenje",
-        
+
             "class": "feed",
             "favorites": true
         },
@@ -165,7 +228,7 @@ dataObj = {
             "id": "7",
             "name": "Barnsley",
             "type": "fudbalski klub",
-    
+
             "favorites": true
         }, {
             "id": "7",
@@ -211,7 +274,7 @@ dataObj = {
 }
 var i = 1
 for (i in dataObj.data) {
-    results += (`<tr class="content"><td>${i}</td><td class="tag-name"><a href="#">${dataObj.data[i].name}</a></td><td>${dataObj.data[i].type}</td><td class='${dataObj.data[i].class === "feed" ? "feed" : ""}' >${dataObj.data[i].class === "feed" ? "yes" : "no"}</td><td class='${dataObj.data[i].favorites === true ? "favorite": ""}'></td><td class="option-box"><button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button></td></tr>`)
+    results += (`<tr class="content"><td>${i}</td><td class="tag-name editable-columns" ><a href="#">${dataObj.data[i].name}</a></td><td class="editable-columns">${dataObj.data[i].type}</td><td class='${dataObj.data[i].class === "feed" ? "feed editable-columns" : "no-feed editable-columns"}' >${dataObj.data[i].class === "feed" ? "yes" : "no"}</td><td class='${dataObj.data[i].favorites === true ? "favorite editable-columns" : "editable-columns"}'></td><td class="option-box"><button class="edit-btn edit-values"></button> <button class="delete-btn"></button></td></tr>`)
 }
 
 $('table').find('tbody').html(results);
